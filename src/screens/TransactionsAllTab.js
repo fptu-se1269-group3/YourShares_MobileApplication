2bdc135d58425f157777e85770d66f67d9b1633e
Cao Van Phu <caovanphu1998@gmail.com>
2019-07-05 12:27:12 +0700

    add fillter of screen transaction

--- src/screens/TransactionsAllTab.js
+++ src/screens/TransactionsAllTab.js
@@ -2,36 +2,81 @@ import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { ScrollView } from 'react-native';
 import { Spinner, Card, CardItem, Body } from "native-base";
-import colors from '../values/Colors'
+import colors from '../values/Colors';
+import jslinq from 'jslinq';
 
 export default class TransactionsAllTab extends Component {
     constructor(props) {
         super(props);
-        console.log(props)
+
+        this.state = {
+            arrayResult: [],
+            firstTime: true,
+        }
+    }
+
+    componentDidUpdate(oldProps) {
+        const newProps = this.props;
+        if (oldProps.selected !== newProps.selected || oldProps.date !== newProps.date || oldProps.date2 !== newProps.date2) {
+            var fromDate = this.props.date != undefined ? new Date(this.props.date.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3")).getTime() / 1000.0 : 0;
+            var toDate = isNaN((new Date(this.props.date2.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3")).getTime() / 1000.0))
+                ? new Date().getTime() / 1000.0
+                : new Date(this.props.date2.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3")).getTime() / 1000.0;
+            if (this.props.selected == 'all') {
+                var myList = this.props.arrayTransaction;
+                var queryObj = jslinq(myList)
+                    .where(function (item) { return item.transactionDate >= fromDate })
+                    .where(function (item) { return item.transactionDate <= toDate })
+                    .toList();
+                this.setState({
+                    arrayResult: queryObj
+                });
+            }
+            else {
+                var shareAccountIds = this.props.arrayShareAccount[this.props.selected];
+                var myList = this.props.arrayTransaction;
+
+
+                var queryObj = jslinq(myList)
+                    .where(function (item) { return item.shareAccountId == shareAccountIds })
+                    .where(function (item) { return item.transactionDate >= fromDate })
+                    .where(function (item) { return item.transactionDate <= toDate })
+                    .toList();
+                this.setState({
+                    arrayResult: queryObj,
+                });
+            }
+        } else if (!this.props.isLoading && this.state.firstTime) {
+            this.setState({
+                arrayResult: this.props.arrayTransaction,
+                firstTime: false
+            });
+
+        }
+
     }
 
     renderCards() {
         const card = [];
-        for (let i = 0; i < this.props.arrayTransaction.length; i++) {
+        for (let i = 0; i < this.state.arrayResult.length; i++) {
             // if (Platform.OS === 'ios') {
             card.push(
-                <TouchableOpacity key={this.props.arrayTransaction[i].transactionId}>
+                <TouchableOpacity key={this.state.arrayResult[i].transactionId}>
                     <View>
                         <Card style={{ borderRadius: 10 }} pointerEvents="none">
                             <CardItem bordered>
                                 <Body>
                                     <Text>
-                                        Type: {this.props.arrayTransaction[i].transactionTypeCode}
-
+                                        Type: {this.state.arrayResult[i].transactionTypeCode}
                                     </Text>
                                     <Text>
-                                        transactionDate: {new Date(this.props.arrayTransaction[i].transactionDate * 1000).toLocaleString()}
+                                        transactionDate: {new Date(this.state.arrayResult[i].transactionDate * 1000).toLocaleString()}
                                     </Text>
                                     <Text>
-                                        transactionAmount: {this.props.arrayTransaction[i].transactionAmount}
+                                        transactionAmount: {this.state.arrayResult[i].transactionAmount}
                                     </Text>
                                     <Text>
-                                        transactionValue: {this.props.arrayTransaction[i].transactionValue}
+                                        transactionValue: {this.state.arrayResult[i].transactionValue}
                                     </Text>
                                 </Body>
                             </CardItem>
